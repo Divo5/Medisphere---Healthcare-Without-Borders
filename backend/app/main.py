a9b87dbc76fc5ebed30c3fcf800233fb9d3fd2f3
Divo5 <anghandivyesh@gmail.com>
2026-05-02 02:00:15 +0530

    added link

--- backend/app/main.py
+++ backend/app/main.py
@@ -54,7 +54,7 @@ app = FastAPI(
 # ── Middleware ───────────────────────────────────────────────────────────────
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "*"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://medisphere-healthcare-without-borders.onrender.com", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
